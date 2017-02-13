3d4cbc72bfb45a53b0673264b02b5c4888b20a7d
Olivaldo Filho <olivaldo@gmail.com>
2017-02-13 14:25:57 -0200

    add

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var http = require('http');
 var app = require('./config/express.js')();
 
-http.createServer(app).listen(app.get('port'), function(){
-    console.log("Express Server rodando na porta " + app.get('port'));
+http.createServer(app).listen(process.env.PORT || 10001, function(){
+    console.log("Express Server rodando na porta " + process.env.PORT || 10001);
 });
