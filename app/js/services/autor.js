707b2fcc0873af285702197499186d91af8f080f
Olivaldo Filho <olivaldo@gmail.com>
2017-02-16 09:00:49 -0200

    nova versão

--- app/js/services/autor.js
+++ app/js/services/autor.js
@@ -1,3 +1,4 @@
 angular.module('biblioteca_client').factory('Autor', function($resource){
-    return $resource('http://localhost:10000/api/autores/:id');
+    //return $resource('http://localhost:10000/api/autores/:id');
+    return $resource('https://bibliotecassiaserver.herokuapp.com/api/autores/:id');
 })
--- app/js/services/livro.js
+++ app/js/services/livro.js
@@ -1,3 +1,4 @@
 angular.module('biblioteca_client').factory('Livro', function($resource){
-    return $resource('http://localhost:10000/api/livros/:idLivro');
+    //return $resource('http://localhost:10000/api/livros/:idLivro');
+    return $resource('https://bibliotecassiaserver.herokuapp.com/api/livros/:idLivro');
 })
