cdd08747e14c41181c96c17b782a97a0c4c64b54
Olivaldo Filho <olivaldo@gmail.com>
2017-02-24 11:52:37 -0300

    melhorando a interface

--- app/js/controllers/autor.js
+++ app/js/controllers/autor.js
@@ -2,8 +2,7 @@ angular.module('biblioteca_client')
     .controller('autor', function(Autor, $scope, $resource, $routeParams, $cookies){
     //var Livro = $resource('http://localhost:10000/api/livro/:idLivro');
     //var LivroSalva = $resource('http://localhost:10000/api/livro/');
-    
-                             
+                                 
     novo();
 
     $scope.novo = function(){
@@ -12,83 +11,54 @@ angular.module('biblioteca_client')
     };
 
     function novo(){
-        $scope.mensagem = {texto: ''};
-        $scope.mensagemErro = {
-            texto: '',
-            status: ''
-        };                   
-        $scope.autor = new Autor();        
+        $scope.mensagem = {texto: '', status: ''};                         
+        $scope.autor = new Autor();
     };
-
     
     $scope.delete = function(){
         var id = $scope.autor.id;
         if (!id){
-            $scope.mensagem = {texto: 'Selecione um cadastro válido! '}; 
+            $scope.mensagem = {texto: 'Selecione um cadastro válido! ', status: ''}; 
             return;
         }
         Autor.delete({id}, function(autor) {
-            $scope.mensagem = {texto: 'Autor excluído com sucesso! '};            
+            $scope.mensagem = {texto: 'Autor excluído com sucesso! ', status: ''};            
 			$scope.autor = autor;
 		}, 
 		function(erro) {		    
-			$scope.mensagemErro = {texto: 'Erro ao excluir autor. ',
-                                   status: erro};            
+			$scope.mensagem = {texto: 'Erro ao excluir autor. ', status: erro};
 		});        
-    }
+    };
 
     $scope.salva = function(){
         console.log($scope.autor);
         $scope.autor.$save()        
             .then(function(autor){
-                $scope.mensagem = {texto: 'Autor cadastrado com sucesso! '};
+                $scope.mensagem = {texto: 'Autor cadastrado com sucesso', status: ''};
 			    $scope.autor = autor;        
             })
             .catch(function(erro){
                 console.log(erro.status);
-                $scope.mensagemErro = {texto: 'Erro ao gravar autor. ',
-                                   status: erro};
-                $scope.autor = new Autor();                 
-             });
-    };        
+                $scope.mensagem = {texto: 'Erro ao gravar autor. ', status: erro};
+                $scope.autor = new Autor();                                
+            });
+    };
+
+    $scope.pesquisa = function(){
+        window.location.href = "#/autores/";       
+    }
+
     var id = $routeParams.id;
     if (id){        
         Autor.get({id}, function(autor) {
-            $scope.mensagem = {texto: 'Autor encontrado! ' + id};            
+            //$scope.mensagem = {texto: 'Autor encontrado! ' + id, status: ''};            
 			$scope.autor = autor;
 		}, 
 		function(erro) {		    
-			$scope.mensagem = {texto: 'Autor não existe. Novo contato. '};
+			$scope.mensagem = {texto: 'Autor não existe. Novo contato. ', status: ''};
             $scope.autor = new Autor();
 		});    
     } else {
-        //$scope.mensagem = {texto: 'Contato não existe. Novo contato. '};
         $scope.autor = new Autor();        
     };
 });
-
-
-//LivroSalva.save($scope.livro)        
-        //LivroSalva.save({$scope.contato});        
-        /*LivroSalva.save($scope.livro, function(livro){            
-            $scope.mensagem = {texto: 'Contrato encontrado! ' + idLivro};
-			$scope.livro = livro;
-            //console.log(res.statusCode);
-            },
-            function(erro){
-                console.log(erro);
-			    $scope.mensagem = {texto: 'Erro ao gravar contato. ' + erro};
-                $scope.livro = new Livro();
-            }
-        );
-
-        /*$scope.contato.$save()
-            .then(function(){
-                $scope.mensagem = {texto: 'Salvo com sucesso'};
-                $scope.contato = new Contato();
-             })
-             .catch(function(erro){
-                 $scope.mensagem = {texto: 'Não foi possível salvar'};
-             });
-        };         
-           */
